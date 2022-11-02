8cef5f374e00f9744e477782c7594eccf1b58fa2
David Schramm <david.schramm@live.com>
2022-11-02 11:25:50 +0100

    first working version

--- ep_ldapauth_ng.js
+++ ep_ldapauth_ng.js
@@ -12,18 +12,66 @@ var settings = require('ep_etherpad-lite/node/utils/Settings');
 var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 
 function ldapauthSetUsername(token, username) {
-  console.debug('ep_ldapauth.ldapauthSetUsername: getting authorid for token %s', token);
+  console.debug('ep_ldapauth_ng.ldapauthSetUsername: getting authorid for token %s', token);
   let author = authorManager.getAuthor4Token(token)
   author.then(
     function(author_id) {
-      console.debug('ep_ldapauth.ldapauthSetUsername: have authorid %s, setting username to "%s"', author_id, username);
+      console.debug('ep_ldapauth_ng.ldapauthSetUsername: have authorid %s, setting username to "%s"', author_id, username);
       authorManager.setAuthorName(author_id, username);
     });
   return;
 }
 
+
+function user_is_admin(username, userDN, cb){
+   var authopts= {
+      url: settings.users.ldapauth.url,
+      adminDn: settings.users.ldapauth.searchDN,
+      adminPassword: settings.users.ldapauth.searchPWD,
+      searchBase: settings.users.ldapauth.accountBase,
+      searchFilter: settings.users.ldapauth.accountPattern,
+      groupSearchBase: settings.users.ldapauth.groupSearchBase,
+      groupAttribute: settings.users.ldapauth.groupAttribute,
+      groupAttributeIsDN: settings.users.ldapauth.groupAttributeIsDN,
+      searchScope: settings.users.ldapauth.searchScope,
+      groupSearch: settings.users.ldapauth.groupSearch,
+      cache: true
+    };
+
+    if (typeof(settings.users.ldapauth.tls_ca_file) !== 'undefined') {
+      authops.tls_ca = fs.readFileSync(settings.users.ldapauth.tls_ca_file);
+    }
+
+    var ldap_con = new MyLdapAuth(authopts);
+
+    ldap_con.groupsearch(username, userDN, function(err, groups) {
+		if(err){
+			console.debug("ep_ldapauth_ng.user_is_admin: error while searching: %s", err);
+			return;
+		}
+
+		if(groups){
+			for(let i=0;i<groups.length;++i){
+				if(groups[i].dn==settings.users.ldapauth.adminGroupDN){
+					return cb(true);
+				}
+			}
+		}else{
+			console.debug("no groups");
+		}
+	
+		ldap_con.close(function (err) {
+        	if (err) {
+        	  console.error('ep_ldapauth_ng.user_is_admin: LDAP close error: %s', err);
+        	}
+    	});
+	});
+
+	return cb(false);
+}
+
 exports.authenticate = function(hook_name, context, cb) {
-  console.debug('ep_ldapauth.authenticate');
+  console.debug('ep_ldapauth_ng.authenticate');
   // If auth headers are present use them to authenticate
   if (context.req.headers.authorization && context.req.headers.authorization.search('Basic ') === 0) {
     var userpass = new Buffer(context.req.headers.authorization.split(' ')[1], 'base64').toString().split(":");
@@ -48,14 +96,14 @@ exports.authenticate = function(hook_name, context, cb) {
     // Attempt to authenticate the user
     authenticateLDAP.authenticate(username, password, function(err, user) {
       if (err) {
-        console.error('ep_ldapauth.authenticate: LDAP auth error: %s', err);
+        console.error('ep_ldapauth_ng.authenticate: LDAP auth error: %s', err);
         authenticateLDAP.close(function (err) {
           if (err) {
-            console.error('ep_ldapauth.authenticate: LDAP close error: %s', err);
+            console.error('ep_ldapauth_ng.authenticate: LDAP close error: %s', err);
           }
         });
         authenticateLDAP = null;
-        return cb([false]);
+        return cb(false);
       }
 
       // User authenticated, save off some information needed for authorization
@@ -70,29 +118,39 @@ exports.authenticate = function(hook_name, context, cb) {
         context.req.session.user.userDN = user.dn;
       }
       settings.globalUserName = username;
-      console.debug('ep_ldapauth.authenticate: deferring setting of username [%s] to CLIENT_READY for express_sid = %s', username, express_sid);
+      console.debug('ep_ldapauth_ng.authenticate: deferring setting of username [%s] to CLIENT_READY for express_sid = %s', username, express_sid);
       authenticateLDAP.close(function (err) {
         if (err) {
-          console.error('ep_ldapauth.authenticate: LDAP close error: %s', err);
+          console.error('ep_ldapauth_ng.authenticate: LDAP close error: %s', err);
         }
       });
       authenticateLDAP = null;
-      console.debug('ep_ldapauth.authenticate: successful authentication');
-      return cb([true]);
+
+	  user_is_admin(username, user.dn, function (r){
+		if(r){
+			console.debug("user %s is an admin!", username);
+        	context.req.session.user.is_admin = true;
+	  	}else{
+			console.debug("user %s is not an admin!", username);
+		}
+	  });
+
+      console.debug('ep_ldapauth_ng.authenticate: successful authentication');
+      return cb(context.req.session.user.is_admin);
     });
   } else {
-    console.debug('ep_ldapauth.authenticate: failed authentication no auth headers');
-    return cb([false]);
+    console.debug('ep_ldapauth_ng.authenticate: failed authentication no auth headers');
+    return cb(false);
   }
 };
 
 exports.authorize = function(hook_name, context, cb) {
-  console.debug('ep_ldapauth.authorize');
+  console.debug('ep_ldapauth_ng.authorize');
 
   if(settings.users.ldapauth.anonymousReadonly &&
       /^\/(p\/r\..{16}|locales.json|static|javascripts|pluginfw|favicon.ico)/.test(context.resource)) {
-    console.debug('ep_ldapauth.authorize.anonymous: authorizing static path %s', context.resource);
-    return cb([true]);
+    console.debug('ep_ldapauth_ng.authorize.anonymous: authorizing static path %s', context.resource);
+    return cb(true);
   }
 
   userDN = null;
@@ -104,15 +162,15 @@ exports.authorize = function(hook_name, context, cb) {
       userDN = context.req.session.user.userDN;
     }
   } else {
-    console.debug('ep_ldapauth.authorize: no username in user object');
-    return cb([false]);
+    console.debug('ep_ldapauth_ng.authorize: no username in user object');
+    return cb(false);
   }
 
   if (/^\/(static|javascripts|pluginfw|favicon.ico|api)/.test(context.resource)) {
-    console.debug('ep_ldapauth.authorize: authorizing static path %s', context.resource);
-    return cb([true]);
+    console.debug('ep_ldapauth_ng.authorize: authorizing static path %s', context.resource);
+    return cb(true);
   } else if (context.resource.match(/^\/admin/)) {
-    console.debug('ep_ldapauth.authorize: attempting to authorize along administrative path %s', context.resource);
+    console.debug('ep_ldapauth_ng.authorize: attempting to authorize along administrative path %s', context.resource);
     var myLdapAuthOpts = {
       url: settings.users.ldapauth.url,
       adminDn: settings.users.ldapauth.searchDN,
@@ -135,66 +193,73 @@ exports.authorize = function(hook_name, context, cb) {
 
     authorizeLDAP.groupsearch(username, userDN, function(err, groups) {
       if (err) {
-        console.error('ep_ldapauth.authorize: LDAP groupsearch error: %s', err);
+        console.error('ep_ldapauth_ng.authorize: LDAP groupsearch error: %s', err);
         authorizeLDAP.close(function (err) {
           if (err) {
-            console.error('ep_ldapauth.authorize: LDAP close error: %s', err);
+            console.error('ep_ldapauth_ng.authorize: LDAP close error: %s', err);
           }
         });
         authorizeLDAP = null;
-        return cb([false]);
+        return cb(false);
       }
 
       // We've recieved back group(s) that the user matches
       // Given our current auth scheme (only checking on admin) we'll auth
       if (groups) {
-		console.log(groups)
-        context.req.session.user.is_admin = true;
-        authorizeLDAP.close(function (err) {
+        
+        context.req.session.user.is_admin = false;
+		for(let i=0;i<groups.length;++i){
+			if(groups[i].dn==settings.users.ldapauth.adminGroupDN){
+        		context.req.session.user.is_admin = true;
+				break;
+			}
+		}
+
+		authorizeLDAP.close(function (err) {
           if (err) {
-            console.error('ep_ldapauth.authorize: LDAP close error: %s', err);
+            console.error('ep_ldapauth_ng.authorize: LDAP close error: %s', err);
           }
         });
         authorizeLDAP = null;
-        console.debug('ep_ldapauth.authorize: successful authorization');
-        return cb([true]);
+        console.debug('ep_ldapauth_ng.authorize: successful authorization');
+        return cb(true);
       } else {
         context.req.session.user.is_admin = false;
         authorizeLDAP.close(function (err) {
           if (err) {
-            console.error('ep_ldapauth.authorize: LDAP close error: %s', err);
+            console.error('ep_ldapauth_ng.authorize: LDAP close error: %s', err);
           }
         });
         authorizeLDAP = null;
-        console.debug('ep_ldapauth.authorize: failed authorization');
-        return cb([false]);
+        console.debug('ep_ldapauth_ng.authorize: failed authorization');
+        return cb(false);
       }
     });
   } else {
-    console.debug('ep_ldapauth.authorize: passing authorize along for path %s', context.resource);
-    return cb([false]);
+    console.debug('ep_ldapauth_ng.authorize: passing authorize along for path %s', context.resource);
+    return cb(false);
   }
 };
 
 exports.handleMessage = function(hook_name, context, cb) {
-  console.debug("ep_ldapauth.handleMessage");
+  console.debug("ep_ldapauth_ng.handleMessage");
   if ( context.message.type == "CLIENT_READY" ) {
     if (!context.message.token) {
-      console.debug('ep_ldapauth.handleMessage: intercepted CLIENT_READY message has no token!');
+      console.debug('ep_ldapauth_ng.handleMessage: intercepted CLIENT_READY message has no token!');
     } else {
       var client_id = context.client.id;
       if ('user' in context.client.client.request.session) {
         var displayName = context.client.client.request.session.user.displayName;
         if(settings.users.ldapauth.anonymousReadonly && !displayName) displayName = 'guest';
-        console.debug('ep_ldapauth.handleMessage: intercepted CLIENT_READY message for client_id = %s, setting username for token %s to %s', client_id, context.message.token, displayName);
+        console.debug('ep_ldapauth_ng.handleMessage: intercepted CLIENT_READY message for client_id = %s, setting username for token %s to %s', client_id, context.message.token, displayName);
         ldapauthSetUsername(context.message.token, displayName);
       }
       else {
-        console.debug('ep_ldapauth.handleMessage: intercepted CLIENT_READY but user does have displayName !');
+        console.debug('ep_ldapauth_ng.handleMessage: intercepted CLIENT_READY but user does have displayName !');
       }
     }
   } else if ( context.message.type == "COLLABROOM" && context.message.data.type == "USERINFO_UPDATE" ) {
-    console.debug('ep_ldapauth.handleMessage: intercepted USERINFO_UPDATE and dropping it!');
+    console.debug('ep_ldapauth_ng.handleMessage: intercepted USERINFO_UPDATE and dropping it!');
     return cb([null]);
   }
   return cb([context.message]);
